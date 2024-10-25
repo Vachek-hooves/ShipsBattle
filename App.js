e1649f364f2123b11b9f1118b2b2075ccdb1717e
Vachek-hooves <vachek0602@gmail.com>
2024-10-25 17:03:50 +0300

    create static screen

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppContextProvider } from './store/context';
 import WelcomeScreen from './screen/Stack/StackWelcomeScreen';
-import { TabHarborScreen, TabQuizScreen, TabShipsBattle } from './screen/Tab';
+import { TabHarborScreen, TabQuizScreen, TabShipsBattle, TabStatistickScreen } from './screen/Tab';
 import { StackQuizScreen } from './screen/Stack';
 import { StackShipsBattle } from './screen/Stack';
 const Stack = createNativeStackNavigator();
@@ -21,6 +21,7 @@ const TabNavigator = () => {
       <Tab.Screen name="TabQuizScreen" component={TabQuizScreen} />
       <Tab.Screen name="TabHarborScreen" component={TabHarborScreen} />
       <Tab.Screen name="TabShipsBattle" component={TabShipsBattle} />
+      <Tab.Screen name="TabStatistickScreen" component={TabStatistickScreen} />
     </Tab.Navigator>
   );
 };
