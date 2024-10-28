691c3e0124c2c2632bfe60a1e7c0e5d12b977053
Vachek-hooves <vachek0602@gmail.com>
2024-10-28 08:48:49 +0200

    update

---
+++ components/userSoundControl/soundIcon.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SoundIcon = () => {
+  return (
+    <View>
+      <Text>soundIcon</Text>
+    </View>
+  )
+}
+
+export default SoundIcon
+
+const styles = StyleSheet.create({})
